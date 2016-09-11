0f59eb7b6464fc2152c5afb33cf67eeb1803e88f
Marcin Skawiński <marcin@moreawarefly.com>
2016-09-11 20:34:23 +0200

    added list sorting, refactored albums view, scss refactor

--- src/js/modules/albums.js
+++ src/js/modules/albums.js
@@ -8,8 +8,16 @@
     function AlbumsController(dataservice) {
         const vm = this;
 
+        vm.sortAttribute = 'name';
+        vm.sortDescending = false;
+        vm.toggleSortingOrder = toggleSortingOrder;
+
         dataservice.getAlbums().then((response) => {
             vm.albums = response;
         });
+
+        function toggleSortingOrder() {
+            vm.sortDescending = !vm.sortDescending;
+        }
     }
 })();
