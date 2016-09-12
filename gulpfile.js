0ecea5623c7a1a733f586b8219898d58a4ec6cce
Marcin Skawiński <marcin@moreawarefly.com>
2016-09-12 21:47:18 +0200

    added production build with build serving task

--- gulpfile.js
+++ gulpfile.js
@@ -5,24 +5,36 @@ const connect = require('gulp-connect');
 const concat = require('gulp-concat');
 const templateCache = require('gulp-angular-templatecache');
 const es = require('event-stream');
-const cors = (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    setTimeout(next, 1000);
-};
+const htmlmin = require('gulp-htmlmin');
+const inlinesource = require('gulp-inline-source');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+
 
 gulp.task('default', ['devServer']);
+gulp.task('build', ['prepareBuild'], build);
+gulp.task('serveBuild', ['build', 'serveMock'], serveBuild);
 
+
+gulp.task('prepareBuild', [
+    'populateTmpDirDist',
+    'concatJsDist',
+    'transpileScssDist',
+]);
 gulp.task('devServer', [
-    'populateTmpDir',
-    'concatJs',
-    'transpileScss',
+    'populateTmpDirDev',
+    'concatJsDev',
+    'transpileScssDev',
     'watch',
     'serveTmp',
     'serveMock',
 ]);
-gulp.task('concatJs', concatJs);
-gulp.task('populateTmpDir', populateTmpDir);
-gulp.task('transpileScss', transpileScss);
+gulp.task('populateTmpDirDist', populateTmpDirDist);
+gulp.task('concatJsDist', concatJsDist);
+gulp.task('transpileScssDist', transpileScssDist);
+gulp.task('concatJsDev', concatJsDev);
+gulp.task('populateTmpDirDev', populateTmpDirDev);
+gulp.task('transpileScssDev', transpileScssDev);
 gulp.task('serveTmp', serveTmp);
 gulp.task('serveMock', serveMock);
 gulp.task('reload', reload);
@@ -37,10 +49,12 @@ gulp.task('watch', [
 
 function compileTemplates() {
     return gulp.src('./src/views/**/*.html').
-        pipe(templateCache());
+        pipe(htmlmin({
+            collapseWhitespace: true,
+        })).pipe(templateCache());
 }
 
-function concatJs() {
+function concatJsDev() {
     return es.merge(gulp.src([
         './src/js/app.js',
         './.tmp/js/templates.js',
@@ -51,18 +65,45 @@ function concatJs() {
         pipe(sourcemaps.write()).
         pipe(gulp.dest('.tmp/js'));
 }
+function concatJsDist() {
+    return es.merge(gulp.src([
+        './src/js/app.js',
+        './.tmp/js/templates.js',
+        './src/js/**/*.js',
+    ]), compileTemplates()).
+        pipe(concat('app.js')).
+        pipe(babel({
+            presets: ['es2015'],
+        })).
+        pipe(uglify()).
+        pipe(gulp.dest('.tmp/js'));
+}
 
-function populateTmpDir() {
+function populateTmpDirDev() {
     gulp.src([
         './src/index.html',
     ]).pipe(gulp.dest('.tmp/'));
 }
+function populateTmpDirDist() {
+    gulp.src([
+        './src/index.html',
+    ]).pipe(htmlmin({
+        collapseWhitespace: true,
+    })).pipe(gulp.dest('.tmp/'));
+}
 
-function transpileScss() {
+function transpileScssDev() {
     gulp.src('./src/css/css.scss').
         pipe(sourcemaps.init()).
         pipe(sass().on('error', sass.logError)).
         pipe(sourcemaps.write()).
+        pipe(gulp.dest('./.tmp/css'));
+}
+function transpileScssDist() {
+    gulp.src('./src/css/css.scss').
+        pipe(sass({
+            outputStyle: 'compressed',
+        }).on('error', sass.logError)).
         pipe(gulp.dest('./.tmp/css'));
 }
 
@@ -81,6 +122,12 @@ function serveMock() {
         middleware: () => [cors],
     });
 }
+function serveBuild() {
+    connect.server({
+        port: 9000,
+        root: './dist',
+    });
+}
 
 function reload() {
     gulp.src('./.tmp/*').
@@ -90,18 +137,29 @@ function reload() {
 function watchHtml() {
     gulp.watch([
         './src/*.html',
-    ], ['populateTmpDir', 'reload']);
+    ], ['populateTmpDirDev', 'reload']);
 }
 
 function watchAngular() {
     gulp.watch([
         './src/js/**/*.js',
         './src/views/**/*.html',
-    ], ['concatJs', 'reload']);
+    ], ['concatJsDev', 'reload']);
 }
 
 function watchSass() {
     gulp.watch([
         './src/css/*.scss',
-    ], ['transpileScss', 'reload']);
+    ], ['transpileScssDev', 'reload']);
+}
+
+function build() {
+    return gulp.src('./.tmp/index.html')
+        .pipe(inlinesource())
+        .pipe(gulp.dest('./dist'));
+}
+
+function cors(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    setTimeout(next, 1000);
 }
